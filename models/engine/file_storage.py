eb8e2fc780a30bf48552995514c96645a7d9973a
dinma773-3 <godsfavourchukwudi21@gmail.com>
2023-09-21 14:38:46 +0000

    updated files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,57 +1,80 @@
 #!/usr/bin/python3
-"""class to manage storage"""
+"""This is the file storage class for AirBnB"""
 import json
-import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import shlex
 
 
 class FileStorage:
-"""storage of models"""
-
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Returns models"""
-        if (not cls):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
             return self.__objects
-        result = {}
-        for key in self.__objects.keys():
-            if (key.split(".")[0] == cls.__name__):
-                result.update({key: self.__objects[key]})
-        return result
 
     def new(self, obj):
-        """new object added"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """update JSON file"""
-        temp = {}
-        for id, obj in self.__objects.items():
-            temp[id] = obj.to_dict()
-        with open(self.__file_path, "w") as json_file:
-            json.dump(temp, json_file)
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """
-        update dict
+        """serialize the file path to JSON file path
         """
         try:
-            with open(self.__file_path, "r") as json_file:
-                temp = json.load(json_file)
-            for id, dict in temp.items():
-                temp_instance = models.dummy_classes[dict["__class__"]](**dict)
-                self.__objects[id] = temp_instance
-        except:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
             pass
 
-    def close(self):
-        """display data"""
-        self.reload()
-
     def delete(self, obj=None):
-        """deletes if needed
+        """ delete an existing element
         """
-        if (obj):
-            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
+
+    def close(self):
+        """ calls reload()
+        """
+        self.reload()
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,5 @@
--- Creates a database hbnb_test_db
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- creates user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Grants Permissions for user
-GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
